4d13c6fd9f7e7d31aba1978d78873b1f5652393d
fikretellek <fikretellek@icloud.com>
2024-11-30 17:44:33 +0000

    card dimensions adjusted for sub pages

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,8 +6,10 @@ const Contact = () => {
         <div id="contact-hero">
           <div id="active-card-color"></div>
           <div id="active-card" className="sub-page-card">
-            I’m always open to connecting—whether it’s to build the next great app or chat about the
-            latest in AI. Let’s create something amazing together!
+            <div id="inner-card" className="active-inner">
+              I’m always open to connecting—whether it’s to build the next great app or chat about
+              the latest in AI. Let’s create something amazing together!
+            </div>
           </div>
           <div id="image-area" className="sub-page-card">
             abcd
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -3,13 +3,17 @@ import "./Projects.scss";
 const Products = () => {
   return (
     <>
-      <div id="products-hero">
+      <div id="products-hero" className="active-page">
         <div id="active-card-color"></div>
         <div id="active-card" className="sub-page-card">
-          abcd
+          <div id="inner-card" className="active-inner">
+            abcd
+          </div>
         </div>
         <div id="image-area" className="sub-page-card">
-          abcd
+          <div id="inner-card" className="active-inner">
+            abcd
+          </div>
         </div>
       </div>
     </>
