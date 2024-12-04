0d501e1e85edf91e0431e6fed3eaee318ba7e41d
fikretellek <fikretellek@icloud.com>
2024-12-04 18:44:13 +0000

    Hash routing added instead of browser routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Header from "./components/Header/Header";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
@@ -7,23 +7,32 @@ import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import WaveBackground from "./components/WaveBackground/WaveBackground";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(window.location.pathname);
+    if (window.location.pathname !== "/" && !window.location.pathname.startsWith("/#")) {
+      window.location.pathname = "/";
+    }
+  }, [navigate]);
+
   return (
     <>
       <WaveBackground />
-      <BrowserRouter>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
+
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
     </>
   );
 }
