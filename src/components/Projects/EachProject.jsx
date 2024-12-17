aade12fa90581ff17b4e57be469ccb6b1734ed41
fikretellek <fikretellek@icloud.com>
2024-12-17 19:42:42 +0000

    project images added to project card

--- src/components/Projects/EachProject.jsx
+++ src/components/Projects/EachProject.jsx
@@ -1,9 +1,22 @@
+import appointment from "../../assets/appoint.png";
+import readme from "../../assets/read.png";
+import aut from "../../assets/aut.png";
+import mess from "../../assets/message.png";
+import map from "../../assets/map.png";
+import b1 from "../../assets/b1.jpg";
 const EachProject = ({ id }) => {
+  const photos = [appointment, readme, aut, mess, map];
+  const backgrounds = [b1];
   return (
     <>
       <div className={"project-container " + (id % 2 == 1 ? "reversed-flex-direction" : "")}>
         <div id={"project-" + id} className="top-margin-for-header"></div>
-        <div className="project-image-container"></div>
+        <div style={{ backgroundImage: backgrounds[id - 1] }} className="project-image-container">
+          <div className="background-img-elm" id={"b" + id}>
+            .
+          </div>
+          <img src={photos[id - 1]} alt="" />
+        </div>
         <div className="project-info-container">
           <h1>asdcasdcasc</h1>
           <h2>sacasdcasd</h2>
